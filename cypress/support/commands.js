e247f722d1246ef1bf2a7156107e2c08492aa8fb
ABHISHEK Chhawari <abhishekch360@gmail.com>
2025-02-01 17:14:12 +0530

    Cypress : Custom commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,20 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('login',(username,password)=>{
+    cy.visit("/loginurl")
+    cy.xpath("//input-username").type(username)
+    cy.xpath("//input-password").type(password)
+    cy.xpath("login-btn").click({force:true})
+    cy.url().should('contain','/dashboard') // Now we can use cy.login inside test files
+})
+
+Cypress.Commands.add('loginfrom fixture',()=>{
+    cy.fixture('testdata').then((data)=>{
+        cy.visit('loginurl');
+        cy.xpath("//input-user").type(data['username']);
+        cy.xpath("//input-password").type(data['password']);
+        cy.xpath("login-btn").click(); // We can also overwrite commands using cypress.commands.overwrite
+    });
+});
