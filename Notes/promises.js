b6a2183410ccd8bc1fe258a39603ba17c71969dc
ABHISHEK Chhawari <abhishekch360@gmail.com>
2025-02-03 00:15:04 +0530

    Promises:all

--- Notes/promises.js
+++ Notes/promises.js
@@ -1,4 +1,4 @@
-let variable = false;
+let variable = true;
 function userVar(){
     return new Promise((resolve,reject)=>{
         if (variable){
@@ -20,4 +20,25 @@ userVar().then(({key,val})=>{
     console.log(key,val);
 }).catch(({key,val})=>{
     console.log(key,val);
+});
+
+//Promises All
+const task1 = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve("Task 1 is completed");
+    }, 2000);
+});
+
+const task2 = new Promise((resolve,reject)=>{
+    resolve("Task 2 is completed");
+});
+
+const task3 = new Promise((resolve,reject)=>{
+    resolve("Task 3 is completed");
+});
+
+Promise.all([task1,task2,task3]).then((messages)=>{
+    console.log("messages array is ", messages);
+}).catch((err)=>{
+    console.log("Error occured ",err);
 });
